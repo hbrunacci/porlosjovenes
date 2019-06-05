a39d320905eb08d60d6705025d95daed61109f36
Hernan <hbrunacci@gmail.com>
2019-06-05 15:32:58 -0300

    fix views.py on search

--- search/views.py
+++ search/views.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append("..")
 
 
-
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import render
 from wagtail.core.models import Page
 from wagtail.search.models import Query
