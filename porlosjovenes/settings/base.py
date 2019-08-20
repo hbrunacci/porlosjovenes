76f4d6aeed0f8baabe03ba58a809116c58fac193
Hernan <hbrunacci@gmail.com>
2019-08-20 20:23:18 -0300

    disable 2FA

--- porlosjovenes/settings/base.py
+++ porlosjovenes/settings/base.py
@@ -52,7 +52,7 @@ INSTALLED_APPS = [
     "wagtail.contrib.routable_page",
     'wagtail.contrib.settings',
     'wagtailmetadata',
-    'wagtail_2fa',
+     #'wagtail_2fa',
     'django_otp',
     'django_otp.plugins.otp_totp',
     #'wagtailcaptcha',
