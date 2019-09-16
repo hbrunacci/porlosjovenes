365d04f36217140d5e43e8bba13bc42bfb664268
Hernan <hbrunacci@gmail.com>
2019-09-16 10:46:44 -0300

    Pause CSP to fix permission

--- porlosjovenes/settings/base.py
+++ porlosjovenes/settings/base.py
@@ -77,7 +77,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'csp.middleware.CSPMiddleware',
+    #'csp.middleware.CSPMiddleware',
 
     'wagtail.core.middleware.SiteMiddleware',
     'wagtail.contrib.redirects.middleware.RedirectMiddleware',
